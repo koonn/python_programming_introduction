610271f2405571d47e3f6a79b8a8d89a34cfe889
Konno Takahiro <konnie211@gmail.com>
2024-07-13 09:26:06 +0900

    add: 関数の仕様を変更

--- src/odd_even.py
+++ src/odd_even.py
@@ -1,17 +1,27 @@
-def min_odd(x, y, z):
-    if x%2 != 0 and y%2 != 0 and z%2 != 0:
-        return min(x, y, z)
-    elif x%2 != 0 and y%2 != 0 and z%2 == 0:
-        return min(x, y)
-    elif x%2 != 0 and y%2 == 0 and z%2 != 0:
-        return min(x, z)
-    elif x%2 == 0 and y%2 != 0 and z%2 != 0:
-        return min(y, z)
-    elif x%2 != 0 and y%2 == 0 and z%2 == 0:
-        return x
-    elif x%2 == 0 and y%2 != 0 and z%2 == 0:
-        return y
-    elif x%2 == 0 and y%2 == 0 and z%2 != 0:
-        return z
-    else:
-        return min(x, y, z)
+def max_odd_or_min_even(x, y, z):
+    answer = min(x, y, z)
+    if x%2 != 0:
+        answer = x
+    if y%2 != 0 and y > answer:
+        answer = y
+    if z%2 != 0 and z > answer:
+        answer = z
+    return answer
+
+# def max_odd_or_min_even(x, y, z):
+#     if x%2 != 0 and y%2 != 0 and z%2 != 0:
+#         return max(x, y, z)
+#     elif x%2 != 0 and y%2 != 0 and z%2 == 0:
+#         return max(x, y)
+#     elif x%2 != 0 and y%2 == 0 and z%2 != 0:
+#         return max(x, z)
+#     elif x%2 == 0 and y%2 != 0 and z%2 != 0:
+#         return max(y, z)
+#     elif x%2 != 0 and y%2 == 0 and z%2 == 0:
+#         return x
+#     elif x%2 == 0 and y%2 != 0 and z%2 == 0:
+#         return y
+#     elif x%2 == 0 and y%2 == 0 and z%2 != 0:
+#         return z
+#     else:
+#         return max(x, y, z)
--- src/tests/test_odd_even.py
+++ src/tests/test_odd_even.py
@@ -1,15 +1,15 @@
 import pytest
-from ..odd_even import min_odd
+from ..odd_even import max_odd_or_min_even
 
 @pytest.mark.parametrize("x, y, z, expected", [
-    (1, 3, 5, 1), # all odd
-    (2, 3, 5, 3), # x even
-    (3, 2, 5, 3), # y even
-    (5, 3, 2, 3), # z even
+    (1, 3, 5, 5), # all odd
+    (2, 3, 5, 5), # x even
+    (3, 2, 5, 5), # y even
+    (5, 3, 2, 5), # z even
     (2, 4, 5, 5), # x and y even
     (3, 4, 2, 3), # y and z even
     (2, 3, 4, 3), # x and z even
     (2, 4, 6, 2), # all even
 ])
-def test_min_odd(x, y, z, expected):
-    assert min_odd(x, y, z) == expected
+def test_max_odd_or_min_even(x, y, z, expected):
+    assert max_odd_or_min_even(x, y, z) == expected
