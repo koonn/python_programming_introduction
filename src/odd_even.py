02a1eed7f875ccdc1f0d2ba8be309a76cc66cc72
Konno Takahiro <konnie211@gmail.com>
2024-07-13 09:27:23 +0900

    add: 不要な関数を削除

--- src/odd_even.py
+++ src/odd_even.py
@@ -7,21 +7,3 @@ def max_odd_or_min_even(x, y, z):
     if z%2 != 0 and z > answer:
         answer = z
     return answer
-
-# def max_odd_or_min_even(x, y, z):
-#     if x%2 != 0 and y%2 != 0 and z%2 != 0:
-#         return max(x, y, z)
-#     elif x%2 != 0 and y%2 != 0 and z%2 == 0:
-#         return max(x, y)
-#     elif x%2 != 0 and y%2 == 0 and z%2 != 0:
-#         return max(x, z)
-#     elif x%2 == 0 and y%2 != 0 and z%2 != 0:
-#         return max(y, z)
-#     elif x%2 != 0 and y%2 == 0 and z%2 == 0:
-#         return x
-#     elif x%2 == 0 and y%2 != 0 and z%2 == 0:
-#         return y
-#     elif x%2 == 0 and y%2 == 0 and z%2 != 0:
-#         return z
-#     else:
-#         return max(x, y, z)
